89098936ba0b40a3a47c380104e6c8b1fc19edff
mixxuss <mixxuss@gmail.com>
2017-02-17 13:50:26 +0300

    Save

--- metrika_report.py
+++ metrika_report.py
@@ -1,4 +1,11 @@
 import requests
+from openpyxl import Workbook
+
+
+def parse_input_file(path):
+    with open(path, 'r') as text:
+        text_lines_list = text.read().splitlines()
+        return [text_line.split(' ') for text_line in text_lines_list]
 
 
 def request_metrika_api_compare(params, url='https://api-metrika.yandex.ru/stat/v1/data/comparison'):
@@ -25,12 +32,12 @@ def make_output_params(metrics='ym:s:visits', dimensions='ym:s:<attribution>Sour
     }
 
 
-def make_date_params(start_date_a, finish_date_a, start_date_b, finish_date_b):
+def make_date_params(dates_list):
     return {
-        'date1_a': start_date_a,
-        'date2_a': finish_date_a,
-        'date1_b': start_date_b,
-        'date2_b': finish_date_b
+        'date1_a': dates_list[0],
+        'date2_a': dates_list[1],
+        'date1_b': dates_list[2],
+        'date2_b': dates_list[3]
     }
 
 
@@ -50,43 +57,51 @@ def group_params(base_params, output_params, date_params, filter_params):
     return all_params
 
 
-def make_filter(section_list, traffic_source='organic'):
-    minus_words = make_minus_word_param()
-    return '(ym:s:trafficSource==\'{}\') AND {} AND {}'.format(traffic_source, minus_words, '{}'.format([section for section in section_list]))
+def summ_filter(section):
+    return '(ym:s:trafficSource==\'organic\') AND (ym:s:searchPhrase!@\'baby\' AND ym:s:searchPhrase!@\'бэби\' AND ym:s:searchPhrase!@\'беби\') AND {}'.format(make_any_filter(section))
 
 
-def make_minus_word_param(minus_words_list=['baby', 'беби', 'бэби']):
-    if len(minus_words_list) > 1:
-        minus_words_str = ' AND '.join(['ym:s:searchPhrase!@\'{}\''.format(minus_word) for minus_word in minus_words_list])
-        return '({})'.format(minus_words_str)
-    else:
-        return ['ym:s:searchPhrase!@\'{}\''.format(minus_word) for minus_word in minus_words_list]
-
-
-def make_sections_param(section_list=['/community/*']):
-    if len(section_list) > 1:
-        section_param = ' AND '.join(['ym:s:startURLPathFull=*\'{}\''.format(section) for section in section_list])
+def make_any_filter(param_list):
+    if len(param_list) > 1:
+        section_param = ' AND '.join(['{}{}\'{}\''.format(params[0], params[1], params[2]) for params in param_list])
         return '({})'.format(section_param)
     else:
-        return '(ym:s:startURLPathFull=*\'{}\')'.format(section_list[0])
+        for params in param_list:
+            return '({}{}\'{}\')'.format(params[0], params[1], params[2])
 
 
-def make_params_list(params):
+def output_to_xlsx():
+    pass
+
+def combine_params_list(params):
     pass
 
 
 if __name__ == '__main__':
-    print(make_minus_word_param())
-    print(make_sections_param())
+    dates = parse_input_file('dates.txt')
+    sections = parse_input_file('sections.txt')
+    print(dates)
+    print(sections)
+    '''for date in dates:
+        for section in sections:
+            print(group_params(base_params=make_base_params(),
+                               output_params=make_output_params(),
+                               date_params=make_date_params(date),
+                               filter_params=make_filter_params(summ_filter([section]), summ_filter([section]) + ' AND (ym:u:totalVisitsDuration>=180)')))
+    '''
+    params = {'dimensions': 'ym:s:<attribution>SourceEngine', 'filters_b': "(ym:s:trafficSource=='organic') AND (ym:s:searchPhrase!@'baby' AND ym:s:searchPhrase!@'бэби' AND ym:s:searchPhrase!@'беби') AND (ym:s:startURLPathFull=*'/community/*') AND (ym:u:totalVisitsDuration>=180)", 'pretty': 'true', 'limit': 100, 'oauth_token': 'AQAAAAAT07WxAAQP6j2oJLgK80E0sM-fbCEx6eo', 'date2_a': '2017-01-29', 'date1_b': '2017-01-23', 'date1_a': '2017-01-23', 'metrics': 'ym:s:visits', 'date2_b': '2017-01-29', 'group': 'week', 'ids': '26444823', 'accuracy': 'high', 'filters_a': "(ym:s:trafficSource=='organic') AND (ym:s:searchPhrase!@'baby' AND ym:s:searchPhrase!@'бэби' AND ym:s:searchPhrase!@'беби') AND (ym:s:startURLPathFull=*'/community/*')"}
+    print(request_metrika_api_compare(params))
+
+
 
 
     '''
     dates_list = [
-        {'date1_a': '2017-02-09', 'date2_a': '2017-02-10', 'date1_b': '2017-02-09', 'date2_b': '2017-02-10'},
-        {'date1_a': '2017-02-06', 'date2_a': '2017-02-08', 'date1_b': '2017-02-06', 'date2_b': '2017-02-08'}
+        {'date1_a': '2017-01-23', 'date2_a': '2017-01-29', 'date1_b': '2017-01-23', 'date2_b': '2017-01-29'},
+        {'date1_a': '2017-02-06', 'date2_a': '2017-02-12', 'date1_b': '2017-02-06', 'date2_b': '2017-02-12'}
     ]
-    section_list = [{'ym:s:startURLPathFull=*':'/community/*'},
-                    {'ym:s:startURLPathFull=*':'/answers/*'}]
+    section_list = [['ym:s:startURLPathFull' '=*', '/community/*'],
+                    ['ym:s:startURLPathFull', '=*', '/answers/*']]
 
     params = group_params(make_base_params(),
                           make_output_params(),
